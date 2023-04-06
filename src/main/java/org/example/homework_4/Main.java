de04967db3bc1fcc31cfa6284056b3b9dadd5c5b
Vagazzi <nikita19961104@gmail.com>
2023-04-06 19:19:28 +0300

    completed 5th home-task, waiting for a code review

--- src/main/java/org/example/homework_4/Main.java
+++ src/main/java/org/example/homework_4/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.homework_4;
 
 import java.util.Arrays;
 import java.util.Random;
---
+++ src/main/java/org/example/homework_5/Status.java
@@ -0,0 +1,6 @@
+package org.example.homework_5;
+
+public class Status {
+    public static boolean STATUS_OK = true;
+    public static boolean STATUS_NOT = false;
+}
