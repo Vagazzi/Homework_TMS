52e2cfe1aea21fe70cc99bba3bd7066f56959541
Vagazzi <nikita19961104@gmail.com>
2023-05-09 11:19:58 +0300

    10th hometask

---
+++ src/main/java/org/example/homework/tenth/Exceptions/InvalidEndingException.java
@@ -0,0 +1,7 @@
+package org.example.homework.tenth.Exceptions;
+
+public class InvalidEndingException extends Exception{
+    public InvalidEndingException(String message){
+        super(message);
+    }
+}
