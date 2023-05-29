fec802d396425640aeaf125ae34c62d731ff5d99
Vagazzi <nikita19961104@gmail.com>
2023-05-30 00:38:04 +0300

    edited after a code review

--- src/main/java/org/example/homework/fifteenth/FifteenthHometask.java
+++ src/main/java/org/example/homework/fifteenth/FifteenthHometask.java
@@ -1,12 +1,12 @@
 package org.example.homework.fifteenth;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FifteenthHometask {
     public static void main(String[] args) {
-        List<Book> library = addBooks();
+        List<Book> library = initializeLibrary();
         sortByYear(library);
         groupByAuthor(library);
         countBookByYear(library);
@@ -14,7 +14,7 @@ public class FifteenthHometask {
         groupByFirstLetter(library);
     }
 
-    private static List<Book> addBooks() {
+    private static List<Book> initializeLibrary() {
         return List.of(new Book(1990, "Pushkin", "Neonuar"),
                 new Book(1920, "Dostoevsky", "War and peace"),
                 new Book(1812, "Pushkin", "The Godfather"),
@@ -25,42 +25,41 @@ public class FifteenthHometask {
                 new Book(1929, "Dostoevsky", "A Mein Kampf"));
     }
 
-    private static void sortByYear(List<Book> library) {
+    private static List<Book> sortByYear(List<Book> library) {
 
-        library.stream()
+        return library.stream()
                 .sorted(Book::compareToYear)
-                .toList()
-                .forEach(System.out::println);
+                .toList();
 
     }
 
-    private static void groupByAuthor(List<Book> library) {
+    private static Map<String, List<Book>> groupByAuthor(List<Book> library) {
 
-        library.stream()
+        return library.stream()
                 .collect(Collectors.groupingBy(
                         Book::getAuthor));
 
     }
 
-    private static void countBookByYear(List<Book> library) {
+    private static Map<Integer, Long> countBookByYear(List<Book> library) {
 
-        library.stream()
+        return library.stream()
                 .collect(Collectors.groupingBy(
                         Book::getYear, Collectors.counting()));
 
     }
 
-    private static void countBookByAuthor(List<Book> library) {
+    private static Map<String, Long> countBookByAuthor(List<Book> library) {
 
-        library.stream()
+        return library.stream()
                 .collect(Collectors.groupingBy(
                         Book::getAuthor, Collectors.counting()));
 
     }
 
-    private static void groupByFirstLetter(List<Book> library){
+    private static Map<Character, List<Book>> groupByFirstLetter(List<Book> library) {
 
-         library.stream()
+        return library.stream()
                 .collect(Collectors.groupingBy(Book::getFirstLetter));
 
     }
