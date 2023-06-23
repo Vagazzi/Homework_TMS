b24e4e90a89d8161e1df52bfcea07c88f4fa7b2f
Vagazzi <nikita19961104@gmail.com>
2023-06-23 23:40:02 +0300

    renamed (3)

--- src/main/java/org/example/homework/eighteenth/Main.java
+++ src/main/java/org/example/homework/eighteenth/Main.java
@@ -8,20 +8,20 @@ public class Main {
         MySQLConfig msSQL = new MySQLConfig();
 
 
-        List<Student> mySqlStudentsDefQuery = DatabaseHandler.readData(msSQL.getURL(),msSQL.getUser(),msSQL.getPassword(),msSQL.getDefaultQuery(),false);
-        List<Student> mySqlStudentsJoinQuery = DatabaseHandler.readData(msSQL.getURL(),msSQL.getUser(),msSQL.getPassword(),msSQL.getJoinQuery(),true);
+        List<Student> mySqlDefaultStudents = DatabaseHandler.readData(msSQL.getURL(),msSQL.getUser(),msSQL.getPassword(),msSQL.getDefaultQuery(),false);
+        List<Student> mySqlJoinStudents = DatabaseHandler.readData(msSQL.getURL(),msSQL.getUser(),msSQL.getPassword(),msSQL.getJoinQuery(),true);
 
         System.out.println("MySQL queries");
-        System.out.println(mySqlStudentsDefQuery);
-        System.out.println(mySqlStudentsJoinQuery);
+        System.out.println(mySqlDefaultStudents);
+        System.out.println(mySqlJoinStudents);
         System.out.println("--------------");
 
 
-        List<Student> pSqlStudentsDefQuery = DatabaseHandler.readData(psql.getURL(),psql.getUser(),psql.getPassword(),psql.getDefaultQuery(),false);
-        List<Student> pSqlStudentsJoinQuery = DatabaseHandler.readData(psql.getURL(),psql.getUser(),psql.getPassword(),psql.getJoinQuery(),true);
+        List<Student> pSqlDefaultStudents = DatabaseHandler.readData(psql.getURL(),psql.getUser(),psql.getPassword(),psql.getDefaultQuery(),false);
+        List<Student> pSqlJoinStudents = DatabaseHandler.readData(psql.getURL(),psql.getUser(),psql.getPassword(),psql.getJoinQuery(),true);
         System.out.println("PSQL queries");
-        System.out.println(pSqlStudentsDefQuery);
-        System.out.println(pSqlStudentsJoinQuery);
+        System.out.println(pSqlDefaultStudents);
+        System.out.println(pSqlJoinStudents);
     }
 
 
